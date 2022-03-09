eddef8f6458c610af5b0fcf3a706c4cd5f75ede8
admin <ssaannjjuu098@gmail.com>
2022-03-09 16:54:34 +0530

    Sub added

---
+++ repo/src/repo/Sub.java
@@ -0,0 +1,14 @@
+package repo;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		int b = 4;
+		int c = a - b;
+		System.out.println(c);
+
+	}
+
+}
