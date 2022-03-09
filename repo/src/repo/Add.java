ebba18f4773cab410204139c84c4d668cf744084
admin <ssaannjjuu098@gmail.com>
2022-03-09 16:36:24 +0530

    Modified

--- repo/src/repo/Add.java
+++ repo/src/repo/Add.java
@@ -4,7 +4,7 @@ public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 6;
+		int a = 8;
 		int b = 4;
 		int c = a + b;
 		System.out.println(c);
