ac22d1d3ddac5b067a77104b1b586e02898f2242
sanju0987 <101178229+sanju0987@users.noreply.github.com>
2022-03-09 17:53:36 +0530

    Update Add.java

--- repo/src/repo/Add.java
+++ repo/src/repo/Add.java
@@ -4,9 +4,9 @@ public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 2;
-		int b = 2;
-		int c = 2;
+		int a = 3;
+		int b = 3;
+		int c = 3;
 		int d = a + b +c;
 		System.out.println(c);
 		
