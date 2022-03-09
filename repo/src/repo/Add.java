d97c5a184d9befddc6207662db718b306d84c726
admin <ssaannjjuu098@gmail.com>
2022-03-09 16:06:58 +0530

    new class added

---
+++ repo/src/repo/Add.java
@@ -0,0 +1,14 @@
+package repo;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		int b = 4;
+		int c = a + b;
+		System.out.println(c);
+		
+	}
+
+}
